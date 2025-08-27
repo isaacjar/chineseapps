017b019ca4ed7bcecdd8d063789b29a71b303a22
isaacjar <isaacjaramago@gmail.com>
2025-08-27 22:43:24 +0200

    Add files via upload

--- voclists/index.js
+++ voclists/index.js
@@ -1,5 +1,6 @@
 const voclists = [
   { filename: "hsk1", title: "HSK Nivel 1", level: "A1", misc: "Básico" },
   { filename: "viajes", title: "Vocabulario de Viajes", level: "A2", misc: "Turismo" },
+  { filename: "casa", title: "La casa en chino", level: "A3", misc: "Casa" },
   // más listados...
 ];

6e53827e547a282afdc0e103fb61ddfd4eceec1a
isaacjar <isaacjaramago@gmail.com>
2025-08-27 21:43:24 +0200

    Update index.js

--- voclists/index.js
+++ voclists/index.js
@@ -1,1 +1,5 @@
-
+const voclists = [
+  { filename: "hsk1", title: "HSK Nivel 1", level: "A1", misc: "Básico" },
+  { filename: "viajes", title: "Vocabulario de Viajes", level: "A2", misc: "Turismo" },
+  // más listados...
+];

7ed34aaf2d0365b483df84533cb29087065724f2
isaacjar <isaacjaramago@gmail.com>
2025-08-27 23:32:43 +0200

    Add files via upload

--- voclists/index.js
+++ voclists/index.js
@@ -1,6 +1,10 @@
 const voclists = [
-  { filename: "hsk1", title: "HSK Nivel 1", level: "A1", misc: "Básico" },
-  { filename: "viajes", title: "Vocabulario de Viajes", level: "A2", misc: "Turismo" },
-  { filename: "house", title: "La casa en chino", level: "A3", misc: "Casa" },
+  { filename: "H1L1a4", title: "HSK 1 Lessons 1 to 4", level: "H1", misc: "MIT" },
+  { filename: "H1L5", title: "HSK 1 Lesson 5", level: "H1", misc: "MIT" },
+  { filename: "H1L5a6", title: "HSK 1 Lessons 5 & 6", level: "H1", misc: "MIT" },
+  { filename: "viajes", title: "Viajar en chino", level: "H2", misc: "topics" },
+  { filename: "house", title: "La casa en chino", level: "H3", misc: "topics" },
+  { filename: "hsk1", title: "HSK 1 full voc", level: "H1", misc: "hsk" },
+  voclists/hsk1.json
   // más listados...
 ];

a0608baa84dfe75618daae4448d3f15669120f99
isaacjar <isaacjaramago@gmail.com>
2025-08-28 17:14:34 +0200

    Update index.js

--- voclists/index.js
+++ voclists/index.js
@@ -13,6 +13,7 @@ const voclists = [
   { filename: "H1E110034", title: "HSK 1 H11003/4 Vocabulary", level: "H1", misc: "MIT" },
   { filename: "hsk1", title: "HSK 1 Vocabulary (full)", level: "H1", misc: "hsk" },
   { filename: "H2L1a3", title: "HSK 2 Lessons 1 to 3", level: "H2", misc: "MIT" },
+  { filename: "H2L4", title: "HSK 2 Lesson 4", level: "H2", misc: "MIT" },
   { filename: "hsk2", title: "HSK 2 Vocabulary (full)", level: "H2", misc: "hsk" },
   { filename: "H3L1", title: "HSK 3 Lesson 1", level: "H3", misc: "MIT" },
   { filename: "hsk3", title: "HSK 3 Vocabulary (full)", level: "H3", misc: "hsk" },

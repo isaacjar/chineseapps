4553e6b9590875980c69c449abfb3eb3bb18d9d3
Isaac JH <isaacjaramago@gmail.com>
2025-09-04 01:48:50 +0200

    Update chars.js

--- hanzicrack/data/chars.js
+++ hanzicrack/data/chars.js
@@ -54,12 +54,12 @@ export const data = {
     "components": ["亻", "尔"]
   },
   "好": {
-    "pinyin": "nǚ",
-    "meaning_en": "woman",
-    "meaning_es": "mujer",
+    "pinyin": "hǎo",
+    "meaning_en": "good",
+    "meaning_es": "bueno",
     "radical": "女",
-    "strokes": 3,
-    "frequency": 88,
+    "strokes": 6,
+    "frequency": 49,
     "components": ["女", "子"]
   },
   "尔": {
@@ -87,6 +87,609 @@ export const data = {
     "radical": "十",
     "strokes": 2,
     "frequency": 78,
+    "components": []
+  }, 
+  "的": {
+    "pinyin": "de",
+    "meaning_en": "of",
+    "meaning_es": "de",
+    "radical": "白",
+    "strokes": 8,
+    "frequency": 1,
+    "components": ["白", "勺"]
+  },
+  "一": {
+    "pinyin": "yī",
+    "meaning_en": "one",
+    "meaning_es": "uno",
+    "radical": "一",
+    "strokes": 1,
+    "frequency": 2,
+    "components": ["一"]
+  },
+  "是": {
+    "pinyin": "shì",
+    "meaning_en": "to be",
+    "meaning_es": "ser",
+    "radical": "日",
+    "strokes": 9,
+    "frequency": 3,
+    "components": ["日", "正"]
+  },
+  "不": {
+    "pinyin": "bù",
+    "meaning_en": "not",
+    "meaning_es": "no",
+    "radical": "一",
+    "strokes": 4,
+    "frequency": 4,
+    "components": ["一", "丿", "丨"]
+  },
+  "了": {
+    "pinyin": "le",
+    "meaning_en": "completed action",
+    "meaning_es": "acción completada",
+    "radical": "亅",
+    "strokes": 2,
+    "frequency": 5,
+    "components": ["亅"]
+  },
+  "我": {
+    "pinyin": "wǒ",
+    "meaning_en": "I",
+    "meaning_es": "yo",
+    "radical": "戈",
+    "strokes": 7,
+    "frequency": 6,
+    "components": ["戈", "扌"]
+  },
+  "有": {
+    "pinyin": "yǒu",
+    "meaning_en": "to have",
+    "meaning_es": "tener",
+    "radical": "月",
+    "strokes": 6,
+    "frequency": 7,
+    "components": ["又", "月"]
+  },
+  "他": {
+    "pinyin": "tā",
+    "meaning_en": "he",
+    "meaning_es": "él",
+    "radical": "亻",
+    "strokes": 5,
+    "frequency": 8,
+    "components": ["亻", "也"]
+  },
+  "这": {
+    "pinyin": "zhè",
+    "meaning_en": "this",
+    "meaning_es": "esto",
+    "radical": "辶",
+    "strokes": 7,
+    "frequency": 9,
+    "components": ["文", "辶"]
+  },
+  "在": {
+    "pinyin": "zài",
+    "meaning_en": "at",
+    "meaning_es": "en",
+    "radical": "土",
+    "strokes": 6,
+    "frequency": 10,
+    "components": ["土", "才"]
+  },
+  "人": {
+    "pinyin": "rén",
+    "meaning_en": "person",
+    "meaning_es": "persona",
+    "radical": "人",
+    "strokes": 2,
+    "frequency": 26,
+    "components": ["人"]
+  },
+  "都": {
+    "pinyin": "dōu",
+    "meaning_en": "all",
+    "meaning_es": "todos",
+    "radical": "阝",
+    "strokes": 10,
+    "frequency": 27,
+    "components": ["者", "阝"]
+  },
+  "说": {
+    "pinyin": "shuō",
+    "meaning_en": "to speak",
+    "meaning_es": "hablar",
+    "radical": "讠",
+    "strokes": 9,
+    "frequency": 28,
+    "components": ["讠", "兑"]
+  },
+  "想": {
+    "pinyin": "xiǎng",
+    "meaning_en": "to think",
+    "meaning_es": "pensar",
+    "radical": "心",
+    "strokes": 13,
+    "frequency": 29,
+    "components": ["相", "心"]
+  },
+  "看": {
+    "pinyin": "kàn",
+    "meaning_en": "to look",
+    "meaning_es": "mirar",
+    "radical": "目",
+    "strokes": 9,
+    "frequency": 30,
+    "components": ["手", "目"]
+  },
+  "来": {
+    "pinyin": "lái",
+    "meaning_en": "to come",
+    "meaning_es": "venir",
+    "radical": "木",
+    "strokes": 7,
+    "frequency": 31,
+    "components": ["木"]
+  },
+  "给": {
+    "pinyin": "gěi",
+    "meaning_en": "to give",
+    "meaning_es": "dar",
+    "radical": "纟",
+    "strokes": 10,
+    "frequency": 33,
+    "components": ["纟", "合"]
+  },
+  "会": {
+    "pinyin": "huì",
+    "meaning_en": "can / will",
+    "meaning_es": "poder / futuro",
+    "radical": "人",
+    "strokes": 6,
+    "frequency": 34,
+    "components": ["人", "云"]
+  },
+  "去": {
+    "pinyin": "qù",
+    "meaning_en": "to go",
+    "meaning_es": "ir",
+    "radical": "厶",
+    "strokes": 5,
+    "frequency": 35,
+    "components": ["土", "厶"]
+  },
+  "上": {
+    "pinyin": "shàng",
+    "meaning_en": "up / above",
+    "meaning_es": "arriba / encima",
+    "radical": "一",
+    "strokes": 3,
+    "frequency": 37,
+    "components": ["一", "丨"]
+  },
+  "下": {
+    "pinyin": "xià",
+    "meaning_en": "down / below",
+    "meaning_es": "abajo / debajo",
+    "radical": "一",
+    "strokes": 3,
+    "frequency": 38,
+    "components": ["一", "卜"]
+  },
+  "大": {
+    "pinyin": "dà",
+    "meaning_en": "big",
+    "meaning_es": "grande",
+    "radical": "大",
+    "strokes": 3,
+    "frequency": 39,
+    "components": ["一", "人"]
+  },
+  "为": {
+    "pinyin": "wèi",
+    "meaning_en": "for",
+    "meaning_es": "para",
+    "radical": "丶",
+    "strokes": 4,
+    "frequency": 40,
+    "components": ["丶", "力"]
+  },
+  "中": {
+    "pinyin": "zhōng",
+    "meaning_en": "middle",
+    "meaning_es": "medio",
+    "radical": "丨",
+    "strokes": 4,
+    "frequency": 41,
+    "components": ["丨", "口"]
+  },
+  "小": {
+    "pinyin": "xiǎo",
+    "meaning_en": "small",
+    "meaning_es": "pequeño",
+    "radical": "小",
+    "strokes": 3,
+    "frequency": 42,
+    "components": ["小"]
+  },
+  "天": {
+    "pinyin": "tiān",
+    "meaning_en": "sky / day",
+    "meaning_es": "cielo / día",
+    "radical": "大",
+    "strokes": 4,
+    "frequency": 43,
+    "components": ["一", "大"]
+  },
+  "年": {
+    "pinyin": "nián",
+    "meaning_en": "year",
+    "meaning_es": "año",
+    "radical": "干",
+    "strokes": 6,
+    "frequency": 44,
+    "components": ["干", "丿"]
+  },
+  "日": {
+    "pinyin": "rì",
+    "meaning_en": "sun / day",
+    "meaning_es": "sol / día",
+    "radical": "日",
+    "strokes": 4,
+    "frequency": 45,
+    "components": ["日"]
+  },
+  "时": {
+    "pinyin": "shí",
+    "meaning_en": "time / hour",
+    "meaning_es": "hora / tiempo",
+    "radical": "日",
+    "strokes": 7,
+    "frequency": 46,
+    "components": ["日", "寸"]
+  },
+  "多": {
+    "pinyin": "duō",
+    "meaning_en": "many / much",
+    "meaning_es": "mucho / muchos",
+    "radical": "夕",
+    "strokes": 6,
+    "frequency": 48,
+    "components": ["夕", "夕"]
+  },
+  "见": {
+    "pinyin": "jiàn",
+    "meaning_en": "to see",
+    "meaning_es": "ver",
+    "radical": "见",
+    "strokes": 4,
+    "frequency": 50,
+    "components": ["目", "儿"]
+  },
+  "回": {
+    "pinyin": "huí",
+    "meaning_en": "to return",
+    "meaning_es": "volver",
+    "radical": "囗",
+    "strokes": 6,
+    "frequency": 52,
+    "components": ["囗", "口"]
+  },
+  "出": {
+    "pinyin": "chū",
+    "meaning_en": "to go out",
+    "meaning_es": "salir",
+    "radical": "凵",
+    "strokes": 5,
+    "frequency": 53,
+    "components": ["凵", "山"]
+  },
+  "生": {
+    "pinyin": "shēng",
+    "meaning_en": "to be born / life",
+    "meaning_es": "nacer / vida",
+    "radical": "生",
+    "strokes": 5,
+    "frequency": 54,
+    "components": ["生"]
+  },
+  "用": {
+    "pinyin": "yòng",
+    "meaning_en": "to use",
+    "meaning_es": "usar",
+    "radical": "用",
+    "strokes": 5,
+    "frequency": 55,
+    "components": ["用"]
+  },
+  "作": {
+    "pinyin": "zuò",
+    "meaning_en": "to do / to make",
+    "meaning_es": "hacer",
+    "radical": "亻",
+    "strokes": 7,
+    "frequency": 56,
+    "components": ["亻", "乍"]
+  },
+  "事": {
+    "pinyin": "shì",
+    "meaning_en": "matter / affair",
+    "meaning_es": "asunto / cosa",
+    "radical": "亅",
+    "strokes": 8,
+    "frequency": 57,
+    "components": ["口", "亅", "一"]
+  },
+  "里": {
+    "pinyin": "lǐ",
+    "meaning_en": "inside",
+    "meaning_es": "dentro",
+    "radical": "里",
+    "strokes": 7,
+    "frequency": 58,
+    "components": ["田", "土"]
+  },
+  "家": {
+    "pinyin": "jiā",
+    "meaning_en": "home / family",
+    "meaning_es": "hogar / familia",
+    "radical": "宀",
+    "strokes": 10,
+    "frequency": 59,
+    "components": ["宀", "豕"]
+  },
+  "学": {
+    "pinyin": "xué",
+    "meaning_en": "to study / to learn",
+    "meaning_es": "estudiar / aprender",
+    "radical": "子",
+    "strokes": 8,
+    "frequency": 60,
+    "components": ["冖", "子", "⺍"]
+  },
+  "开": {
+    "pinyin": "kāi",
+    "meaning_en": "to open",
+    "meaning_es": "abrir",
+    "radical": "门",
+    "strokes": 4,
+    "frequency": 61,
+    "components": ["一", "廾"]
+  },
+  "电": {
+    "pinyin": "diàn",
+    "meaning_en": "electric / electricity",
+    "meaning_es": "eléctrico / electricidad",
+    "radical": "雨",
+    "strokes": 5,
+    "frequency": 63,
+    "components": ["雨", "乚"]
+  },
+  "话": {
+    "pinyin": "huà",
+    "meaning_en": "speech / talk",
+    "meaning_es": "habla / conversación",
+    "radical": "讠",
+    "strokes": 8,
+    "frequency": 64,
+    "components": ["讠", "舌"]
+  },
+  "问": {
+    "pinyin": "wèn",
+    "meaning_en": "to ask",
+    "meaning_es": "preguntar",
+    "radical": "门",
+    "strokes": 6,
+    "frequency": 65,
+    "components": ["门", "口"]
+  },
+  "没": {
+    "pinyin": "méi",
+    "meaning_en": "not / none",
+    "meaning_es": "no / ninguno",
+    "radical": "氵",
+    "strokes": 7,
+    "frequency": 66,
+    "components": ["氵", "殳"]
+  },
+  "走": {
+    "pinyin": "zǒu",
+    "meaning_en": "to walk / to go",
+    "meaning_es": "caminar / ir",
+    "radical": "走",
+    "strokes": 7,
+    "frequency": 68,
+    "components": ["止", "𠂤"]
+  },
+  "知": {
+    "pinyin": "zhī",
+    "meaning_en": "to know",
+    "meaning_es": "saber / conocer",
+    "radical": "矢",
+    "strokes": 8,
+    "frequency": 69,
+    "components": ["矢", "口"]
+  },
+  "高": {
+    "pinyin": "gāo",
+    "meaning_en": "tall / high",
+    "meaning_es": "alto",
+    "radical": "高",
+    "strokes": 10,
+    "frequency": 73,
+    "components": ["亠", "口", "冂", "口"]
+  },
+  "手": {
+    "pinyin": "shǒu",
+    "meaning_en": "hand",
+    "meaning_es": "mano",
+    "radical": "手",
+    "strokes": 4,
+    "frequency": 78,
+    "components": ["手"]
+  },
+  "百": {
+    "pinyin": "bǎi",
+    "meaning_en": "hundred",
+    "meaning_es": "cien",
+    "radical": "白",
+    "strokes": 6,
+    "frequency": 80,
+    "components": ["一", "白"]
+  },
+  "千": {
+    "pinyin": "qiān",
+    "meaning_en": "thousand",
+    "meaning_es": "mil",
+    "radical": "十",
+    "strokes": 3,
+    "frequency": 81,
+    "components": ["十", "丿"]
+  },
+  "儿": {
+    "pinyin": "ér",
+    "meaning_en": "child / son",
+    "meaning_es": "niño / hijo",
+    "radical": "儿",
+    "strokes": 2,
+    "frequency": 82,
+    "components": ["儿"]
+  },
+  "气": {
+    "pinyin": "qì",
+    "meaning_en": "air / gas / energy",
+    "meaning_es": "aire / gas / energía",
+    "radical": "气",
+    "strokes": 4,
+    "frequency": 83,
+    "components": ["气"]
+  },
+  "水": {
+    "pinyin": "shuǐ",
+    "meaning_en": "water",
+    "meaning_es": "agua",
+    "radical": "水",
+    "strokes": 4,
+    "frequency": 84,
+    "components": ["水"]
+  },
+  "火": {
+    "pinyin": "huǒ",
+    "meaning_en": "fire",
+    "meaning_es": "fuego",
+    "radical": "火",
+    "strokes": 4,
+    "frequency": 85,
+    "components": ["火"]
+  },
+  "山": {
+    "pinyin": "shān",
+    "meaning_en": "mountain",
+    "meaning_es": "montaña",
+    "radical": "山",
+    "strokes": 3,
+    "frequency": 86,
+    "components": ["山"]
+  },
+  "土": {
+    "pinyin": "tǔ",
+    "meaning_en": "earth / soil",
+    "meaning_es": "tierra / suelo",
+    "radical": "土",
+    "strokes": 3,
+    "frequency": 87,
+    "components": ["土"]
+  },
+  "白": {
+    "pinyin": "bái",
+    "meaning_en": "white",
+    "meaning_es": "blanco",
+    "radical": "白",
+    "strokes": 5,
+    "frequency": 88,
+    "components": ["白"]
+  },
+  "黑": {
+    "pinyin": "hēi",
+    "meaning_en": "black",
+    "meaning_es": "negro",
+    "radical": "黑",
+    "strokes": 12,
+    "frequency": 89,
+    "components": ["黑"]
+  },
+  "红": {
+    "pinyin": "hóng",
+    "meaning_en": "red",
+    "meaning_es": "rojo",
+    "radical": "纟",
+    "strokes": 6,
+    "frequency": 90,
+    "components": ["纟", "工"]
+  },
+  "绿": {
+    "pinyin": "lǜ",
+    "meaning_en": "green",
+    "meaning_es": "verde",
+    "radical": "纟",
+    "strokes": 13,
+    "frequency": 91,
+    "components": ["纟", "录"]
+  },
+  "黄": {
+    "pinyin": "huáng",
+    "meaning_en": "yellow",
+    "meaning_es": "amarillo",
+    "radical": "黄",
+    "strokes": 11,
+    "frequency": 92,
+    "components": ["黄"]
+  },
+  "扌": {
+    "pinyin": "shǒu",
+    "meaning_en": "hand (compressed)",
+    "meaning_es": "mano (comprimido)",
+    "radical": "手",
+    "strokes": 3,
+    "frequency": 0,
+    "components": []
+  },
+  "文": {
+    "pinyin": "wén",
+    "meaning_en": "writing",
+    "meaning_es": "escritura",
+    "radical": "文",
+    "strokes": 4,
+    "frequency": 80,
+    "components": []
+  },
+  "才": {
+    "pinyin": "cái",
+    "meaning_en": "talent",
+    "meaning_es": "talento",
+    "radical": "才",
+    "strokes": 3,
+    "frequency": 206,
+    "components": []
+  },
+  "者": {
+    "pinyin": "zhě",
+    "meaning_en": "person who",
+    "meaning_es": "la persona que",
+    "radical": "耂",
+    "strokes": 10,
+    "frequency": 134,
+    "components": ["耂", "日"]
+  },
+ "耂": {
+    "pinyin": "lǎo",
+    "meaning_en": "old (compressed)",
+    "meaning_es": "viejo (comprimido)",
+    "radical": "耂",
+    "strokes": 6,
+    "frequency": 125,
     "components": []
   }
 };

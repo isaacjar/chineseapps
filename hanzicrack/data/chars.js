7a84754069df4fb60ed653fa412451ab5282d5d6
Isaac JH <isaacjaramago@gmail.com>
2025-09-04 10:57:31 +0200

    Update chars.js

--- hanzicrack/data/chars.js
+++ hanzicrack/data/chars.js
@@ -1,218 +1,217 @@
 export const data = {
   // Trazos básicos
-    "一": {
-      "pinyin": "yī",
-      "meaning_en": "one / horizontal stroke",
-      "meaning_es": "uno / trazo horizontal",
-      "radical": "一",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "丨": {
-      "pinyin": "shù",
-      "meaning_en": "vertical stroke",
-      "meaning_es": "trazo vertical",
-      "radical": "丨",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "丿": {
-      "pinyin": "piě",
-      "meaning_en": "left-falling stroke",
-      "meaning_es": "trazo descendente izquierdo",
-      "radical": "丿",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "㇏": {
-      "pinyin": "nà",
-      "meaning_en": "right-falling stroke",
-      "meaning_es": "trazo descendente derecho",
-      "radical": "㇏",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "丶": {
-      "pinyin": "diǎn",
-      "meaning_en": "dot",
-      "meaning_es": "punto",
-      "radical": "丶",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "亅": {
-      "pinyin": "gōu",
-      "meaning_en": "hook",
-      "meaning_es": "gancho",
-      "radical": "亅",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "㇀": {
-      "pinyin": "tí",
-      "meaning_en": "rising stroke",
-      "meaning_es": "trazo ascendente",
-      "radical": "㇀",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "乛": {
-      "pinyin": "一亅héng gōu",
-      "meaning_en": "horizontal with hook",
-      "meaning_es": "horizontal con gancho",
-      "radical": "乙",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "乚": {
-      "pinyin": "yǐ",
-      "meaning_en": "second / hidden stroke",
-      "meaning_es": "segundo / trazo oculto",
-      "radical": "乙",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "㇃": {
-      "pinyin": "㇃wò gōu",
-      "meaning_en": "lying hook",
-      "meaning_es": "gancho acostado",
-      "radical": "㇃",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "㇁": {
-      "pinyin": "㇁wān gōu",
-      "meaning_en": "curved hook",
-      "meaning_es": "gancho curvo",
-      "radical": "㇁",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-    "㇂": {
-      "pinyin": "㇂xié gōu",
-      "meaning_en": "slanted hook",
-      "meaning_es": "gancho inclinado",
-      "radical": "㇂",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    }, 
-      "㇄": {
-      "pinyin": "丨㇄shù wān",
-      "meaning_en": "vertical bend",
-      "meaning_es": "curva vertical",
-      "radical": "㇄",
-      "strokes": 1,
-      "frequency": 0,
-      "components": []
-    },
-      "㇅": { 
-        "pinyin": "一㇅héng zhé zhé",
-        "meaning_en": "horizontal double turn",
-        "meaning_es": "doble giro horizontal",
-        "radical": "㇅",
-        "strokes": 2,
-        "frequency": 0,
-        "components": []
-    },
-      "㇊": {
-        "pinyin": "一㇀héng zhé tí",
-        "meaning_en": "horizontal turn with rise",
-        "meaning_es": "giro horizontal con ascenso",
-        "radical": "㇊",
-        "strokes": 2,
-        "frequency": 0,
-        "components": []
-    },
-      "㇍": {
-        "pinyin": "一㇁héng zhé wān",
-        "meaning_en": "horizontal turn with curve",
-        "meaning_es": "giro horizontal con curva",
-        "radical": "㇍",
-        "strokes": 2,
-        "frequency": 0,
-        "components": []
-    },
-      "㇈": {
-        "pinyin": "一㇁亅 héng zhé wān gōu",
-        "meaning_en": "horizontal turn curve hook",
-        "meaning_es": "giro horizontal con curva y gancho",
-        "radical": "㇈",
-        "strokes": 3,
-        "frequency": 0,
-        "components": []
-    },
-      "㇌": {
-        "pinyin": "一丿㇁亅héng piě wān gōu",
-        "meaning_en": "horizontal slant curve hook",
-        "meaning_es": "horizontal con cola, curva y gancho",
-        "radical": "㇌",
-        "strokes": 3,
-        "frequency": 0,
-        "components": []
-    },
-      "㇋": {
-        "pinyin": "一㇅丿héng zhé zhé piě",
-        "meaning_en": "horizontal double turn with slant",
-        "meaning_es": "doble giro horizontal con cola",
-        "radical": "㇋",
-        "strokes": 3,
-        "frequency": 0,
-        "components": []
-    },
-      "㇎": {
-        "pinyin": "一㇅㇅héng zhé zhé zhé",
-        "meaning_en": "horizontal triple turn",
-        "meaning_es": "triple giro horizontal",
-        "radical": "㇎",
-        "strokes": 3,
-        "frequency": 0,
-        "components": []
-    },
-      "㇉": {
-        "pinyin": "丨㇅亅shù zhé zhé gōu",
-        "meaning_en": "vertical double turn with hook",
-        "meaning_es": "doble giro vertical con gancho",
-        "radical": "㇉",
-        "strokes": 3,
-        "frequency": 0,
-        "components": []
-    },
-    "㇇": {
-      "pinyin": "丿㇅piě zhé",
-      "meaning_en": "slant with turn",
-      "meaning_es": "trazo inclinado con giro",
-      "radical": "㇇",
-      "strokes": 2,
-      "frequency": 0,
-      "components": []
-    },
-    "㇆": {
-      "pinyin": "丨亅shù zhé gōu",
-      "meaning_en": "vertical turn with hook",
-      "meaning_es": "giro vertical con gancho",
-      "radical": "㇆",
-      "strokes": 2,
-      "frequency": 0,
-      "components": []
-    },
-          
+  "一": {
+    "pinyin": "héng",
+    "meaning_en": "horizontal stroke",
+    "meaning_es": "trazo horizontal",
+    "radical": "一",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "丨": {
+    "pinyin": "shù",
+    "meaning_en": "vertical stroke",
+    "meaning_es": "trazo vertical",
+    "radical": "丨",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "丿": {
+    "pinyin": "piě",
+    "meaning_en": "left-falling stroke",
+    "meaning_es": "trazo descendente izquierdo",
+    "radical": "丿",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "㇏": {
+    "pinyin": "nà",
+    "meaning_en": "right-falling stroke",
+    "meaning_es": "trazo descendente derecho",
+    "radical": "㇏",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "丶": {
+    "pinyin": "diǎn",
+    "meaning_en": "dot",
+    "meaning_es": "punto",
+    "radical": "丶",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "亅": {
+    "pinyin": "gōu",
+    "meaning_en": "hook",
+    "meaning_es": "gancho",
+    "radical": "亅",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "㇀": {
+    "pinyin": "tí",
+    "meaning_en": "rising stroke",
+    "meaning_es": "trazo ascendente",
+    "radical": "㇀",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "乛": {
+    "pinyin": "héng gōu (一亅)",
+    "meaning_en": "horizontal with hook",
+    "meaning_es": "horizontal con gancho",
+    "radical": "乙",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "乚": {
+    "pinyin": "yǐ (乙)",
+    "meaning_en": "second / hidden stroke",
+    "meaning_es": "segundo / trazo oculto",
+    "radical": "乙",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "㇃": {
+    "pinyin": "wò gōu",
+    "meaning_en": "lying hook",
+    "meaning_es": "gancho acostado",
+    "radical": "㇃",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "㇁": {
+    "pinyin": "wān gōu",
+    "meaning_en": "curved hook",
+    "meaning_es": "gancho curvo",
+    "radical": "㇁",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "㇂": {
+    "pinyin": "xié gōu",
+    "meaning_en": "slanted hook",
+    "meaning_es": "gancho inclinado",
+    "radical": "㇂",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "㇄": {
+    "pinyin": "shù wān (丨㇄)",
+    "meaning_en": "vertical bend",
+    "meaning_es": "curva vertical",
+    "radical": "㇄",
+    "strokes": 1,
+    "frequency": 0,
+    "components": []
+  },
+  "㇇": {
+    "pinyin": "piě zhé (丿㇅)",
+    "meaning_en": "slant with turn",
+    "meaning_es": "trazo inclinado con giro",
+    "radical": "㇇",
+    "strokes": 2,
+    "frequency": 0,
+    "components": []
+  },
+  "㇆": {
+    "pinyin": "shù zhé gōu (丨亅)",
+    "meaning_en": "vertical turn with hook",
+    "meaning_es": "giro vertical con gancho",
+    "radical": "㇆",
+    "strokes": 2,
+    "frequency": 0,
+    "components": []
+  },
+  "㇅": {
+    "pinyin": "héng zhé zhé (一㇅)",
+    "meaning_en": "horizontal double turn",
+    "meaning_es": "doble giro horizontal",
+    "radical": "㇅",
+    "strokes": 2,
+    "frequency": 0,
+    "components": []
+  },
+  "㇊": {
+    "pinyin": "héng zhé tí (一㇀)",
+    "meaning_en": "horizontal turn with rise",
+    "meaning_es": "giro horizontal con ascenso",
+    "radical": "㇊",
+    "strokes": 2,
+    "frequency": 0,
+    "components": []
+  },
+  "㇍": {
+    "pinyin": "héng zhé wān (一㇁)",
+    "meaning_en": "horizontal turn with curve",
+    "meaning_es": "giro horizontal con curva",
+    "radical": "㇍",
+    "strokes": 2,
+    "frequency": 0,
+    "components": []
+  },
+  "㇈": {
+    "pinyin": "héng zhé wān gōu (一㇁亅)",
+    "meaning_en": "horizontal turn curve hook",
+    "meaning_es": "giro horizontal con curva y gancho",
+    "radical": "㇈",
+    "strokes": 3,
+    "frequency": 0,
+    "components": []
+  },
+  "㇌": {
+    "pinyin": "héng piě wān gōu (一丿㇁亅)",
+    "meaning_en": "horizontal slant curve hook",
+    "meaning_es": "horizontal con cola, curva y gancho",
+    "radical": "㇌",
+    "strokes": 3,
+    "frequency": 0,
+    "components": []
+  },
+  "㇋": {
+    "pinyin": "héng zhé zhé piě (一㇅丿)",
+    "meaning_en": "horizontal double turn with slant",
+    "meaning_es": "doble giro horizontal con cola",
+    "radical": "㇋",
+    "strokes": 3,
+    "frequency": 0,
+    "components": []
+  },
+  "㇎": {
+    "pinyin": "héng zhé zhé zhé (一㇅㇅)",
+    "meaning_en": "horizontal triple turn",
+    "meaning_es": "triple giro horizontal",
+    "radical": "㇎",
+    "strokes": 3,
+    "frequency": 0,
+    "components": []
+  },
+  "㇉": {
+    "pinyin": "shù zhé zhé gōu (丨㇅亅)",
+    "meaning_en": "vertical double turn with hook",
+    "meaning_es": "doble giro vertical con gancho",
+    "radical": "㇉",
+    "strokes": 3,
+    "frequency": 0,
+    "components": []
+  }
   // Radicales
   "亻": {
     "pinyin": "rén",
-    "meaning_en": "person",
-    "meaning_es": "persona",
+    "meaning_en": "person (人 compressed)",
+    "meaning_es": "persona (人 comprimido)",
     "radical": "人",
     "strokes": 2,
     "frequency": 120,
@@ -1048,7 +1047,7 @@ export const data = {
     "meaning_es": "gancho",
     "radical": "亅",
     "strokes": 1,
-    "frequency": null,
+    "frequency": 0,
     "components": ["亅"]
   },
   

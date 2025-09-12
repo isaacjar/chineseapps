93838f1e976061e81e744f6266a4f6ee5dbff49d
Isaac JH <isaacjaramago@gmail.com>
2025-09-12 12:17:12 +0200

    Create stats.js

---
+++ hsk4bamboo/js/stats.js
@@ -0,0 +1,14 @@
+// stats.js - Manejo de estadísticas
+class Stats {
+    static init() {
+        // Por ahora vacío, se expandirá en el futuro
+    }
+    
+    static updateStats() {
+        // Por ahora vacío, se expandirá en el futuro
+    }
+    
+    static showStats() {
+        // Por ahora vacío, se expandirá en el futuro
+    }
+}

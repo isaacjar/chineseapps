7626c8c4cec906b5f859f2dead95b0c74ed34c76
Isaac JH <isaacjaramago@gmail.com>
2025-09-24 22:57:12 +0200

    Update index.js

--- voclists/index.js
+++ voclists/index.js
@@ -2,6 +2,7 @@ const voclists = [
   { filename: "H1L1", title: "HSK 1 Lesson 1", level: "H1", misc: "MIT" },
   { filename: "H1L2", title: "HSK 1 Lesson 2", level: "H1", misc: "MIT" },
   { filename: "H1L3", title: "HSK 1 Lesson 3", level: "H1", misc: "MIT" },
+  { filename: "H1L4", title: "HSK 1 Lesson 4", level: "H1", misc: "MIT" },
   { filename: "H1L1a4", title: "HSK 1 Lessons 1 to 4", level: "H1", misc: "MIT" },
   { filename: "H1L5", title: "HSK 1 Lesson 5", level: "H1", misc: "MIT" },
   { filename: "H1L5a6", title: "HSK 1 Lessons 5 & 6", level: "H1", misc: "MIT" },

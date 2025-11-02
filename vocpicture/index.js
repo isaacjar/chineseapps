c555c13816418081804710fcac628a0ae81e94b2
Isaac JH <isaacjaramago@gmail.com>
2025-11-02 18:46:56 +0100

    Update index.js

--- vocpicture/index.js
+++ vocpicture/index.js
@@ -1,3 +1,3 @@
-export const voclists = [
-  { filename: "hsk1_basic_35", title: "HSK 1 Basic (35)", level: "H1", misc: "Isaac" } 
-  ]
+const vocpiclists = [
+  { filename: "hsk1_basic_35", title: "HSK 1 Basic (35)", level: "H1", misc: "Isaac" }
+];

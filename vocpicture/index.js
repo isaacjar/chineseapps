952fa003e3dd48df9fb7204b94d6abe68cc80717
Isaac JH <isaacjaramago@gmail.com>
2025-11-15 00:43:56 +0100

    Add HSK 2 Full entry to vocpiclists

--- vocpicture/index.js
+++ vocpicture/index.js
@@ -1,6 +1,8 @@
 const vocpiclists = [
   { filename: "hsk1_basic_60", title: "HSK 1 Full (60 words)", level: "H1", misc: "Isaac" },
+  { filename: "hsk2_basic_150", title: "HSK 2 Full (150 words)", level: "H2", misc: "Isaac" },
   { filename: "hsk1_basic_35", title: "HSK 1.01 Basic I (35)", level: "H1", misc: "Isaac" },
   { filename: "hsk1_basic2_25", title: "HSK 1.02 Basic II (25)", level: "H1", misc: "Isaac" },
+  { filename: "hsk1_basic2_25", title: "HSK 1.02 Basic II (25)", level: "H1", misc: "Isaac" },  
   { filename: "extras", title: "Extra level", level: "H1", misc: "Isaac" }
 ];

5aa555b1ab5d89c674a7e7b5b8cbee76601a8f2f
Isaac JH <isaacjaramago@gmail.com>
2025-11-30 19:57:24 +0100

    Update index.js

--- vocpicture/index.js
+++ vocpicture/index.js
@@ -2,9 +2,10 @@ const vocpiclists = [
   { filename: "hsk1_basic_60", title: "HSK 1 Full (60 words)", level: "H1", misc: "Isaac" },
   { filename: "hsk2_basic_150", title: "HSK 2 Full (150 words)", level: "H2", misc: "Isaac" }, 
   { filename: "hsk3_full_360", title: "HSK 3 Full (360 words)", level: "H3", misc: "Isaac" },
-  { filename: "hsk1_basic_35", title: "HSK 1.01 Basic I (35)", level: "H1", misc: "Isaac" },
-  { filename: "hsk1_basic2_25", title: "HSK 1.02 Basic II (25)", level: "H1", misc: "Isaac" },
-  { filename: "hsk1_basic2_25", title: "HSK 1.02 Basic II (25)", level: "H1", misc: "Isaac" },  
-  { filename: "body_25", title: "The body (25)", level: "H4", misc: "Isaac" },  
-  { filename: "extras", title: "Extra level (60)", level: "H4", misc: "Isaac" }
+  { filename: "body_30", title: "The body (30 words)", level: "H4", misc: "Isaac" },
+  { filename: "house_35", title: "The house (35 words)", level: "H4", misc: "Isaac" },  
+  { filename: "hsk1_basic_35", title: "HSK 1.01 Basic I (35 words)", level: "H1", misc: "Isaac" },
+  { filename: "hsk1_basic2_25", title: "HSK 1.02 Basic II (25 words)", level: "H1", misc: "Isaac" },
+  { filename: "hsk1_basic2_25", title: "HSK 1.02 Basic II (25 words)", level: "H1", misc: "Isaac" },  
+  { filename: "extras", title: "Extra level (100 words)", level: "H4", misc: "Isaac" }
 ];

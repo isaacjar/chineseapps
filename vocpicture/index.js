0be8da99af10d84dd7b1b6e0d27697f1724b6670
Isaac JH <isaacjaramago@gmail.com>
2025-11-30 20:57:04 +0100

    Update index.js

--- vocpicture/index.js
+++ vocpicture/index.js
@@ -1,4 +1,5 @@
 const vocpiclists = [
+  { filename: "hsk1_basic1_40", title: "HSK 1 A (40 words)", level: "H1", misc: "Isaac" },
   { filename: "hsk1_basic_60", title: "HSK 1 Full (60 words)", level: "H1", misc: "Isaac" },
   { filename: "hsk2_basic_150", title: "HSK 2 Full (150 words)", level: "H2", misc: "Isaac" }, 
   { filename: "hsk3_full_360", title: "HSK 3 Full (360 words)", level: "H3", misc: "Isaac" },
@@ -7,5 +8,5 @@ const vocpiclists = [
   { filename: "hsk1_basic_35", title: "HSK 1.01 Basic I (35 words)", level: "H1", misc: "Isaac" },
   { filename: "hsk1_basic2_25", title: "HSK 1.02 Basic II (25 words)", level: "H1", misc: "Isaac" },
   { filename: "hsk1_basic2_25", title: "HSK 1.02 Basic II (25 words)", level: "H1", misc: "Isaac" },  
-  { filename: "extras", title: "Extra level (100 words)", level: "H4", misc: "Isaac" }
+  { filename: "extras", title: "Extra level (110 words)", level: "H4", misc: "Isaac" }
 ];

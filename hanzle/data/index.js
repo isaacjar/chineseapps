91f26ec3d3e7ae715239388a8f16e45500d31514
Isaac JH <isaacjaramago@gmail.com>
2025-12-21 15:01:57 +0100

    Create index.js

---
+++ hanzle/data/index.js
@@ -0,0 +1,5 @@
+const voclists = [
+  { filename: "h1", title: "HSK 1", level: "H1", misc: "MIT" },
+  { filename: "h2", title: "HSK 2", level: "H2", misc: "MIT" },
+  { filename: "h3", title: "HSK 3", level: "H3", misc: "MIT" }
+]

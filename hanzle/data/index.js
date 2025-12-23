ae57746ed71c40a9a16c20d23990db3ff214ed28
Isaac JH <isaacjaramago@gmail.com>
2025-12-23 20:28:13 +0100

    Update index.js

--- hanzle/data/index.js
+++ hanzle/data/index.js
@@ -1,5 +1,6 @@
 const voclists = [
   { filename: "h1", title: "HSK 1", level: "H1", misc: "MIT" },
   { filename: "h2", title: "HSK 2", level: "H2", misc: "MIT" },
-  { filename: "h3", title: "HSK 3", level: "H3", misc: "MIT" }
+  { filename: "h3", title: "HSK 3", level: "H3", misc: "MIT" },
+  { filename: "h4", title: "HSK 4", level: "H4", misc: "MIT" }
 ]

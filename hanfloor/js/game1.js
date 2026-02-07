8a8491cde600d15b5605d401f81acc86028874ee
Isaac JH <isaacjaramago@gmail.com>
2026-02-07 12:10:13 +0100

    Update game1.js

--- hanfloor/js/game1.js
+++ hanfloor/js/game1.js
@@ -1,4 +1,3 @@
-// game1.js
 const Game1 = {
   vocab: [
     { hanzi: "你好", pinyin: "nǐ hǎo" },
@@ -6,62 +5,23 @@ const Game1 = {
     { hanzi: "再见", pinyin: "zài jiàn" }
   ],
 
-  current: null,
-  waitingAnswer: false,
-
-  start() {
-    this.nextQuestion();
-  },
-
   getQuestion() {
     const item = this.vocab[Math.floor(Math.random() * this.vocab.length)];
     const correct = item.pinyin;
-    const options = [correct];
 
-    while (options.length < 4) {
-      const rnd = this.vocab[Math.floor(Math.random() * this.vocab.length)].pinyin;
-      if (!options.includes(rnd)) options.push(rnd);
-    }
+    // obtener opciones distintas posibles
+    const otherOptions = this.vocab
+      .filter(v => v.pinyin !== correct)
+      .map(v => v.pinyin);
 
-    // mezclar opciones
-    options.sort(() => Math.random() - 0.5);
+    // mezclar y tomar hasta 3 alternativas + la correcta
+    const shuffled = otherOptions.sort(() => Math.random() - 0.5).slice(0, 3);
+    const options = [...shuffled, correct].sort(() => Math.random() - 0.5);
 
     return {
       text: item.hanzi,
       options,
       correct
     };
-  },
-
-  nextQuestion() {
-    this.current = this.getQuestion();
-    this.waitingAnswer = true;
-
-    UI.renderQuestion(
-      currentPlayer,
-      this.current.text,
-      this.current.options,
-      (answer) => this.checkAnswer(answer)
-    );
-  },
-
-  checkAnswer(answer) {
-    if (!this.waitingAnswer) return;
-    this.waitingAnswer = false;
-
-    const isCorrect = answer === this.current.correct;
-
-    if (isCorrect) {
-      UI.playOk();
-      switchPlayer();
-    } else {
-      UI.playFail();
-      UI.penalize(currentPlayer, Settings.data.penalty);
-    }
-
-    // pequeña pausa para feedback visual/sonoro
-    setTimeout(() => {
-      this.nextQuestion();
-    }, 300);
   }
 };

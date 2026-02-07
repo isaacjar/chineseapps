133fc55363c63a29f44f15ae928f09f2169f4b0e
Isaac JH <isaacjaramago@gmail.com>
2026-02-07 12:07:41 +0100

    Create xindex.js

---
+++ voclists/xindex.js
@@ -0,0 +1,5 @@
+const voclists = [
+  { filename: "xH1L1", title: "New HSK 1 Lesson 1", level: "H1", misc: "MIT" },
+  { filename: "H1L2", title: "HSK 1 Lesson 2", level: "H1", misc: "MIT" }
+  // más listados...
+];

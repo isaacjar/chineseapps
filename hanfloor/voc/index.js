5acfca0caf77a17ba67e3659df3e4bf8022fbcd3
Isaac JH <isaacjaramago@gmail.com>
2026-02-08 12:33:04 +0100

    Create index.js

---
+++ hanfloor/voc/index.js
@@ -0,0 +1,6 @@
+const voclists = [
+  { filename: "H1L1a4", title: "HSK 1 01-04", level: "H1", misc: "MIT" },
+  { filename: "H1L5a8", title: "HSK 1 05-08", level: "H1", misc: "MIT" },
+  { filename: "H1L1a8", title: "HSK 1 01-08", level: "H1", misc: "MIT" }
+  // más listados...
+];

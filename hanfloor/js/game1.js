5d52e720d46f65846bcffac100f12e60dfe5fd7d
Isaac JH <isaacjaramago@gmail.com>
2026-02-09 20:11:36 +0100

    d

--- hanfloor/js/game1.js
+++ hanfloor/js/game1.js
@@ -1,4 +1,6 @@
 const Game1 = {
+  mode: "hanzi-to-pinyin",
+
   vocab: [
     { hanzi: "你好", pinyin: "nǐ hǎo" },
     { hanzi: "谢谢", pinyin: "xièxie" },
@@ -7,10 +9,8 @@ const Game1 = {
     { hanzi: "对不起", pinyin: "duìbuqǐ" },
     { hanzi: "没关系", pinyin: "méi guānxi" }
   ],
-
+ 
   getQuestion() {
-    if (!this.vocab || this.vocab.length === 0) return null;
-
     return this.vocab[Math.floor(Math.random() * this.vocab.length)];
   }
 };

9424f2c4d81be04c7bebd21ce222f8fd5961b13b
Isaac JH <isaacjaramago@gmail.com>
2026-02-09 22:06:56 +0100

    Update game1.js

--- hanfloor/js/game1.js
+++ hanfloor/js/game1.js
@@ -11,39 +11,6 @@ const Game1 = {
   ],
 
   getQuestion() {
-    // 1. Elegir palabra base
-    const item = this.vocab[Math.floor(Math.random() * this.vocab.length)];
-    const correct = item.pinyin;
-    const charLength = item.hanzi.length;
-  
-    // 2. Filtrar vocabulario por número de caracteres
-    let sameLengthVocab = this.vocab.filter(
-      v => v.hanzi.length === charLength
-    );
-  
-    // 3. Si no hay suficientes palabras, usar todo el vocabulario
-    if (sameLengthVocab.length < 4) {
-      sameLengthVocab = this.vocab;
-    }
-  
-    // 4. Obtener opciones incorrectas
-    const otherOptions = sameLengthVocab
-      .filter(v => v.pinyin !== correct)
-      .map(v => v.pinyin);
-  
-    // 5. Mezclar y crear opciones
-    const shuffled = otherOptions
-      .sort(() => Math.random() - 0.5)
-      .slice(0, 3);
-  
-    const options = [...shuffled, correct]
-      .sort(() => Math.random() - 0.5);
-  
-    return {
-      text: item.hanzi,   // 👈 se muestra el carácter chino
-      options,            // 👈 pinyin
-      correct
-    };
+    return this.vocab[Math.floor(Math.random() * this.vocab.length)];
   }
-
 };

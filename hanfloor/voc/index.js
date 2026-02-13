3bc26a7e8e8a398f308f0dc8ebdb4b9f470c122b
Isaac JH <isaacjaramago@gmail.com>
2026-02-13 22:55:54 +0100

    Update index.js

--- hanfloor/voc/index.js
+++ hanfloor/voc/index.js
@@ -1,7 +1,11 @@
 const voclists = [
   { filename: "H1L1a4", title: "HSK 1 01-04", level: "H1", misc: "MIT" },
-  { filename: "H1L5a8", title: "HSK 1 05-08", level: "H1", misc: "MIT" },
   { filename: "H1L1a8", title: "HSK 1 01-08", level: "H1", misc: "MIT" },
-  { filename: "hsk1", title: "HSK 1 (full)", level: "H1", misc: "MIT" }
+  { filename: "hsk1", title: "HSK 1 (full)", level: "H1", misc: "MIT" },
+  { filename: "hsk2", title: "HSK 2 (full)", level: "H2", misc: "MIT" },
+  { filename: "hsk3", title: "HSK 3 (full)", level: "H3", misc: "MIT" },
+  { filename: "han01", title: "Characters 01", level: "H1", misc: "MIT" },
+  { filename: "han02", title: "Characters 02", level: "H1", misc: "MIT" },
+  { filename: "han03", title: "Characters 03", level: "H1", misc: "MIT" }  
   // más listados...
 ];

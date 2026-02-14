9bf83c7ca6914d97b3c04dcb3105f05688c58b17
Isaac JH <isaacjaramago@gmail.com>
2026-02-14 13:31:33 +0100

    Enhance getQuestion and buildOptions methods

--- hanfloor/js/game2.js
+++ hanfloor/js/game2.js
@@ -1,8 +1,16 @@
 // game2.js
 const GameMeaning = {
+  mode: "hanzi-to-meaning", // necesario para app.js
+  vocab: [], // se llena desde app.js
+
+  getQuestion() {
+    if (!this.vocab || this.vocab.length === 0) return null;
+    // Elegir palabra aleatoria
+    return this.vocab[Math.floor(Math.random() * this.vocab.length)];
+  },
+
   start(ctx) {
     console.log("Game: Meaning");
-
     this.ctx = ctx;
     this.words = [...ctx.vocab];
     this.nextTurn(1);
@@ -36,7 +44,7 @@ const GameMeaning = {
 
   buildOptions(correct) {
     const opts = new Set([correct]);
-    while (opts.size < 4) {
+    while (opts.size < 4 && this.words.length > 0) {
       const w = this.randomWord();
       opts.add(this.ctx.getMeaning(w));
     }

b3dc87a0b56500773ecdd61a7d7ae65a2e12e571
Isaac JH <isaacjaramago@gmail.com>
2026-02-14 17:27:49 +0100

    Add console log for Game 2 start

--- hanfloor/js/game2.js
+++ hanfloor/js/game2.js
@@ -3,13 +3,16 @@ const GameMeaning = {
   mode: "hanzi-to-meaning",
 
   start(ctx) {
+    console.log("Game 2: Meaning");
     this.ctx = ctx;
     this.vocab = ctx.vocab;
 
+    // 🔑 CRÍTICO
     window.Game = this;
   },
 
   getQuestion() {
+    if (!this.vocab || this.vocab.length === 0) return null;
     return this.vocab[Math.floor(Math.random() * this.vocab.length)];
   }
 };

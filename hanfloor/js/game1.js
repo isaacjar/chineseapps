49cbe76c33052c40079f4f79e72f2b40ccf1da23
Isaac JH <isaacjaramago@gmail.com>
2026-02-14 15:20:37 +0100

    Remove nextTurn and buildOptions methods
    Removed nextTurn and buildOptions methods from the game.

--- hanfloor/js/game1.js
+++ hanfloor/js/game1.js
@@ -10,38 +10,7 @@ const Game1 = {
     window.Game = this;
   },
 
-  nextTurn(player) {
-    const word = this.getQuestion();
-    const correct = word.pinyin;
-
-    const options = this.buildOptions(correct);
-
-    UI.renderQuestion(
-      player,
-      word.hanzi,
-      options,
-      (answer) => {
-        if (answer === correct) {
-          UI.playOk();
-        } else {
-          UI.playFail();
-          UI.markFail(player);
-        }
-      }
-    );
-  },
-
   getQuestion() {
     return this.vocab[Math.floor(Math.random() * this.vocab.length)];
-  },
-
-  buildOptions(correct) {
-    const opts = new Set([correct]);
-
-    while (opts.size < 4) {
-      opts.add(this.getQuestion().pinyin);
-    }
-
-    return [...opts].sort(() => Math.random() - 0.5);
   }
 };

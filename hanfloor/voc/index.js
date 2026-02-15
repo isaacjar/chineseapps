01a206cac35321fabc3fe4fbb84c2729052e1822
Isaac JH <isaacjaramago@gmail.com>
2026-02-15 13:15:58 +0100

    Update index.js

--- hanfloor/voc/index.js
+++ hanfloor/voc/index.js
@@ -6,6 +6,7 @@ const voclists = [
   { filename: "hsk3", title: "HSK 3 (full)", level: "H3", misc: "MIT" },
   { filename: "han01", title: "Basic Hanzis", level: "H1", misc: "MIT" },
   { filename: "han02", title: "First 100 Characters", level: "H2", misc: "MIT" },
+  { filename: "han02b", title: "150 Most Common Characters", level: "H2", misc: "MIT" },
   { filename: "han03", title: "250 Characters", level: "H3", misc: "MIT" }  
   // más listados...
 ];

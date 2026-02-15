dac0794a39e39bf132670958961a63a060835f84
Isaac JH <isaacjaramago@gmail.com>
2026-02-15 16:23:18 +0100

    Update index.js

--- hanfloor/voc/index.js
+++ hanfloor/voc/index.js
@@ -9,6 +9,7 @@ const voclists = [
   { filename: "han01", title: "Basic Hanzis", level: "H1", misc: "MIT" },
   { filename: "han02", title: "First 100 Characters", level: "H2", misc: "MIT" },
   { filename: "han02b", title: "150 Most Common Characters", level: "H2", misc: "MIT" },
+  { filename: "han02c", title: "200 Basic Chinese Characters", level: "H2", misc: "MIT" },
   { filename: "han03", title: "New HSK 1 300 Characters", level: "H3", misc: "MIT" }  
   // más listados...
 ];

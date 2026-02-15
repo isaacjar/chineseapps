8bfeb50e5b0e60934908846e6c8b1f1bffc58036
Isaac JH <isaacjaramago@gmail.com>
2026-02-15 12:46:20 +0100

    Update index.js

--- voclists/index.js
+++ voclists/index.js
@@ -22,6 +22,7 @@ const voclists = [
   { filename: "H1L16", title: "HSK 1 Lesson 16 Wb", level: "H1", misc: "MIT" },
   { filename: "H1E110034", title: "HSK 1 H11003/4 Vocabulary", level: "H1", misc: "MIT" },
   { filename: "hsk1", title: "HSK 1 Vocabulary (full)", level: "H1", misc: "hsk" },
+  { filename: "newhsk1", title: "New HSK 1 (2026)", level: "H1", misc: "hsk" },
   { filename: "H2L1", title: "HSK 2 Lesson 1", level: "H2", misc: "MIT" },
   { filename: "H2L2", title: "HSK 2 Lesson 2", level: "H2", misc: "MIT" },
   { filename: "H2L1a3", title: "HSK 2 Lessons 1 to 3", level: "H2", misc: "MIT" },

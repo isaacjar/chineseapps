8f01ca4ce53d68104f3fe749547ccf5a5f4576a6
Isaac JH <isaacjaramago@gmail.com>
2026-09-19 07:34:14 +0200

    Update HSK entries in voclists

--- hanfloor/voc/index.js
+++ hanfloor/voc/index.js
@@ -1,6 +1,6 @@
 const voclists = [
-  { filename: "H1L1a4", title: "HSK 1 01-04", level: "H1", misc: "MIT" },
-  { filename: "H1L1a8", title: "HSK 1 01-08", level: "H1", misc: "MIT" },
+  { filename: "newH1L1a4", title: "New HSK 1 01-04", level: "H1", misc: "MIT" },
+  { filename: "newH1L1a8", title: "New HSK 1 01-08", level: "H1", misc: "MIT" },
   { filename: "hsk1", title: "HSK 1 (full)", level: "H1", misc: "MIT" },
   { filename: "H2L1a4", title: "HSK 2 01-04", level: "H2", misc: "MIT" },
   { filename: "H2L1a8", title: "HSK 2 01-08", level: "H2", misc: "MIT" },
@@ -19,6 +19,8 @@ const voclists = [
   { filename: "han1000", title: "1000 Chinese Characters", level: "H4", misc: "MIT" },
   { filename: "han1500", title: "1500 Chinese Characters", level: "H5", misc: "MIT" },
   { filename: "han3000", title: "3000 Chinese Characters", level: "H5", misc: "MIT" },
-  { filename: "han5000", title: "5000 Chinese Characters", level: "H6", misc: "MIT" }  
+  { filename: "han5000", title: "5000 Chinese Characters", level: "H6", misc: "MIT" },
+  { filename: "H1L1a4", title: "HSK 1 01-04 (old)", level: "H1", misc: "MIT" },
+  { filename: "H1L1a8", title: "HSK 1 01-08 (old)", level: "H1", misc: "MIT" }
   // más listados...
 ];
